b161dbae1435795d462ee01e725a3c6e623809c2
partialarts <145933508+jemlawX@users.noreply.github.com>
2023-11-26 14:22:03 +0000

    Add dayjs code

--- script/script.js
+++ script/script.js
@@ -1,14 +1,11 @@
+var APIKey = "b34e1ed8e20b43fc4896741bb781c81d";
+var cityInput = $("#search-input");
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey
 
+var date = dayjs().format("(DD/MM/YYYY)");
+console.log(date);
 
-
-
-var getWeather = function() {
-    var APIKey = "b34e1ed8e20b43fc4896741bb781c81d";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
-    var cityInput = $("#search-input");
-}
-
-// // .on("click") function to trigger the fetch() Call
 $("#search-button").on("click", function (event) {
-    event.preventDefault(); // prevent default form behavior
-});
+    event.preventDefault();
+
+})
