49cae977650c1148a01bf2c730681764f4976457
partialarts <145933508+jemlawX@users.noreply.github.com>
2023-11-26 14:38:28 +0000

    Add API call

--- script/script.js
+++ script/script.js
@@ -1,11 +1,22 @@
 var APIKey = "b34e1ed8e20b43fc4896741bb781c81d";
 var cityInput = $("#search-input");
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey
-
 var date = dayjs().format("(DD/MM/YYYY)");
 console.log(date);
 
+var apiCall = function () {
+    var city = cityInput.val().trim();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(queryURL);
+            console.log(data);
+        });
+}
+
 $("#search-button").on("click", function (event) {
     event.preventDefault();
-
+    apiCall();
 })
