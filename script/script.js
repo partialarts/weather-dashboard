b6508011ef804d282b78394c5591cedda3c56a64
partialarts <145933508+jemlawX@users.noreply.github.com>
2023-11-27 15:24:54 +0000

    add API call function and all elements for today section

--- script/script.js
+++ script/script.js
@@ -1,19 +1,29 @@
 var APIKey = "b34e1ed8e20b43fc4896741bb781c81d";
-var cityInput = $("#search-input");
 
 // Get date and time and format with dayjs()
 var date = dayjs().format("(DD/MM/YYYY)");
 
 var getCurrentWeather = function () {
+    var cityInput = $("#search-input");
     var city = cityInput.val().trim();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    fetch(queryURL)
+    var queryCurrentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=metric";
+    var queryFutureURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=metric";
+    fetch(queryCurrentURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(queryURL);
+            console.log(queryCurrentWeather);
             console.log(data);
+            var cityTitle = $("<h2>").text(data.name + " " + date);
+            var currentTemp = $("<p>").text("Temp: "+ data.main.temp + " °C");
+            var currentWind = $("<p>").text("Wind: "+ data.wind.speed + " KM/H");
+            var currentHumidity = $("<p>").text("Humidity: "+ data.main.humidity + "%");
+            var currentWeatherIcon = $("<img src=http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png width=60>");
+            $("#today").append(cityTitle, currentTemp, currentWind, currentHumidity);
+            cityTitle.append(currentWeatherIcon);
+
+            
         });
 }
 
