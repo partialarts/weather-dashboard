66adf7dd65234de37260b7bc3b422460a2360700
partialarts <145933508+jemlawX@users.noreply.github.com>
2023-11-28 18:21:34 +0000

    added additional API call to obtain required data and reorganised code to fix issues

--- script/script.js
+++ script/script.js
@@ -1,48 +1,63 @@
-var APIKey = "b34e1ed8e20b43fc4896741bb781c81d";
-
+var APIKey = "6d61a835b6104a85e78303f92de08a75";
 // Get date and time and format with dayjs()
-var date = dayjs().format("(DD/MM/YYYY)");
+var currentDate = dayjs()
+// Display the current weather
+var showCurrentWeather = function (city, data) {
+    $("#today").empty();
+    $("div").toggleClass("hide", false);
+    console.log (data);
+    $('#today').append(`
+        <h2 id="cityTitle">${city} ${currentDate.format('DD/MM/YYYY')}</h2>
+        <p>Temp: ${data.current.temp} °C</p>
+        <p>Wind: ${data.current.wind_speed} KM/H</p>
+        <p>Humidity: ${data.current.humidity} %</p>
+        `)
+    $('#cityTitle').append(`<img src="https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png" alt="Weather Conditions Icon" width="60">`)
+        
+};
+
+var showForecast = function (data) {
+        // Display the 5-day forecast
+        $("#forecast").empty();
+        $("#forecast").append(`<h3>5-Day Forecast</h3>`);
+        for (i = 1; i < 6; i++) {
+        var date = dayjs.unix(data.daily[i].dt);
+console.log(date);
+                // var dayCards = $("<div>");
+            $('#forecast').append(``);
+
+
+        //         <div class="forecast-cards col-2 mr-3">
+        //             <h5 class="text-center">${dayjs(date).format('MM/DD/YYYY')}</h5>
+        //             <img src="https://openweathermap.org/img/w/${forecast.weather[0].icon}.png" alt="Weather Icon" class="forecastIcon">
+        //             <p>Temperature: ${forecast.main.temp}°F</p>
+        //             <p>Humidity: ${forecast.main.humidity}%</p>
+        //             <p>Wind Speed: ${forecast.wind.speed} m/s</p>
+        //         </div>
+        //         `);
+        //     dayCards.addClass("col-2 mr-3")
+        }
+    };
 
 var getWeather = function () {
-    $("section").empty();
     var cityInput = $("#search-input").val().trim();
-    var queryTodayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey + "&units=metric";
-    var queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + APIKey + "&units=metric";
-
-    Promise.all([ // Method to call both APIs at once
-        fetch(queryTodayURL),
-        fetch(queryForecastURL)
-    ]).then(function (responses) {
-        // Get JSON object from API responses
-        return Promise.all(responses.map(function (response) {
+    var queryGeoURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1&appid=" + APIKey;
+    fetch(queryGeoURL)
+        .then(function (response) {
             return response.json();
-        }));
-    }).then(function (data) {
-        console.log(data);
-
-        // Display the current weather
-        var cityTitle = $("<h2>").text(data[0].name + " " + date);
-        var currentTemp = $("<p>").text("Temp: " + data[0].main.temp + " °C");
-        var currentWind = $("<p>").text("Wind: " + data[0].wind.speed + " KM/H");
-        var currentHumidity = $("<p>").text("Humidity: " + data[0].main.humidity + "%");
-        var currentWeatherIcon = $("<img src=http://openweathermap.org/img/wn/" + data[0].weather[0].icon + "@2x.png width=60>");
-        $("#today").append(cityTitle, currentTemp, currentWind, currentHumidity);
-        cityTitle.append(currentWeatherIcon);
-
-        // Display the 5-day forecast
-        $("#forecast").append($("<h3>").text("5-Day Forecast:"));
-
-        for (i = 0; i < 5; i++) {
-            var dayCards = $("<div>");
-            var cardDates = $("<h4>");
-            var cardIcons = $("<img src=http://openweathermap.org/img/wn/" + data[1].list[0].icon + "@2x.png width=60>");
-
-            dayCards.addClass("col-2 mr-3")
-
-        }
-
-        $("div").toggleClass("hide", false);
-    });
+        }).then(function (geoData) {
+            var lat = geoData[0].lat;
+            var lon = geoData[0].lon;
+            var queryWeatherURL = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=" + APIKey + "&units=metric";
+            fetch(queryWeatherURL)
+                .then(function (response) {
+                    return response.json();
+                }).then(function (weatherData) {
+                    var cityName = geoData[0].name
+                    showCurrentWeather(cityName, weatherData);
+                    showForecast(weatherData)
+                });
+        });
 };
 
 $("#search-button").on("click", function (event) {
